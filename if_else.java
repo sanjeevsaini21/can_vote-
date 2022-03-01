c6fb5fba67bd3da068f80419397f796efe988ec1
Sanjeev Saini <95084610+sanjeevsaini21@users.noreply.github.com>
2022-03-01 21:15:12 +0530

    Create if_else.java
    This program simply tells the user to enter its age and check the voting criteria!!!

---
+++ if_else.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class vote_or_not{
+public static void main(String[] args){
+System.out.println("Age is the a criteria dear");
+Scanner vote = new Scanner(System.in);
+int age = vote.nextInt();
+if(age<=18){
+System.out.println("You can vote!!");
+}
+else{
+System.out.println("You can't vote");
+}
+}}
